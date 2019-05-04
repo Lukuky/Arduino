27fdc844061c4b90da6d262d93c2e5bb1d6bf642
Lukas Hubac <50073534+Lukuky@users.noreply.github.com>
2019-05-04 22:19:01 +0200

    simple blinking

---
+++ LEDs/examples/LED common/LED_blink.ino
@@ -0,0 +1,22 @@
+/*
+  CONNECT DESCRIPTION
+  anode to some pin (digital or analog)
+  katode to GND
+  for +5V logic you need to add resistor!
+*/
+
+int ledPin = 3; // LED name and relevant pin
+byte frequency = 1000; //freguency of blinking
+
+void setup() {
+  pinMode(ledPin, OUTPUT); // set led connected with pin and GND
+}
+
+void loop() {
+  digitalWrite(ledPin, HIGH); // turn on LED
+  delay(frequency); // glowing pause
+  digitalWrite(ledPin, LOW); // turn off LED
+  delay(frequency); // not-glowing pause
+  frequency -= 10; // reduce the frequence by 10
+  //if (frequency == 0) sth; // none-blinking condition
+}
